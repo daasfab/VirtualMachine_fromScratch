15d980f57a17051d08ded7f4f432e379d08ff04a
Daulet <95215177+daasfab@users.noreply.github.com>
2024-12-12 10:48:45 +0000

    Update stack-vm.cpp

--- 🔌 Source Code/stack-vm.cpp
+++ 🔌 Source Code/stack-vm.cpp
@@ -73,6 +73,22 @@ void StackVM::doPrimitive() {
 			memory[sp - 1] = memory[sp - 1] + memory[sp];
 			sp--;
 			break;
+		case 2: // sub
+			std::cout << "sub " << memory[sp - 1] << " " << memory[sp] << std::endl;
+			memory[sp - 1] = memory[sp - 1] - memory[sp];
+			sp--;
+			break;
+		case 3: // mul
+			std::cout << "mul " << memory[sp - 1] << " " << memory[sp] << std::endl;
+			memory[sp - 1] = memory[sp - 1] * memory[sp];
+			sp--;
+
+			break;
+		case 4: // div
+			std::cout << "div " << memory[sp - 1] << " " << memory[sp] << std::endl;
+			memory[sp - 1] = memory[sp - 1] / memory[sp];
+			sp--;
+			break;
 	}
 }
 
